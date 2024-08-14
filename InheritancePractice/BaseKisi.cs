1852c75f32e67301c6393b7bfee7d0cb449c6c4d
auro <auro@DESKTOP-AV8I9BT>
2024-08-14 15:58:53 +0300

    Add project files.

---
+++ InheritancePractice/BaseKisi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    public class BaseKisi
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+    public class Student : BaseKisi
+    {
+        public int StudentNumber { get; set; }
+        public void StudentInfo()
+        {
+            Console.WriteLine($"{StudentNumber} numaralı öğrencimizin adı {FirstName} soyadı {LastName}");
+        }
+
+    }
+    public class Teacher : BaseKisi
+    {
+        public int Salary { get; set; }
+        public void TeacherInfo()
+        {
+            Console.WriteLine($"Öğretmenimizin adı {FirstName} soyadı {LastName} ve maaşı {Salary}");
+
+        }
+    }
+}
---
+++ InheritancePractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using InheritancePractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.FirstName = "Atakan";
+        student.LastName = "Çelik";
+        student.StudentNumber = 233;
+        student.StudentInfo();
+        Teacher teacher = new Teacher();
+        teacher.FirstName = "Fatih";
+        teacher.LastName = "Özkan";
+        teacher.Salary = 45000;
+        teacher.TeacherInfo();
+
+    }
+}
